02310a79d307757982a034f313df213a55a5553b
atmadjal <laura.atmadja@mail.utoronto.ca>
2025-11-14 17:09:36 -0500

    created card properties file

---
+++ properties.py
@@ -0,0 +1,28 @@
+class State:
+    def __init__(self):
+        pass
+
+class Card:
+    def __init__(self):
+        self.type = None
+
+    def choice(self):
+        # types of card: birthday, thank you, Christmas,
+        #                valentine, blank (add your own text)
+        pass
+
+
+class Eyes:
+    pass
+
+
+class Nose:
+    pass
+
+
+class Lips:
+    pass
+
+
+class Accessory:
+    pass
