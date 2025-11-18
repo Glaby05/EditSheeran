be3d8c39c1f888f7026ee4de7593145a6bdae0a1
Michael Chininea <82243647+Twilii@users.noreply.github.com>
2025-11-17 19:09:40 -0500

    Added files for MVC

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import tkinter as tk
-from PIL import ImageTk, Image
-from EditSheeran.views.attribute_frame import *
-
 # Window / Root with fixed size - done sw
 # put Frames as partitions of the app:
 #   - to divide the app into different main parts
@@ -10,57 +6,59 @@ from EditSheeran.views.attribute_frame import *
 #   -> right: (last quarter of window vertically)
 #       -> top:
 #       -> bottom: display the last update of the picture
+#
+# class App(tk.Tk):
+#     def __init__(self):
+#         super().__init__()
+#         self.title("Ed Sheeran Photo Editor")
+#         self.geometry("1200x700")
+#
+#         self.card = None  # shared model
+#
+#         # self.attributes_frame = AttributesFrame()
+#         # self.preview_frame = PreviewFrame(self)
+#         # self.photobook_frame = PhotobookFrame(self)
+#
+#         # self.attributes_frame.pack(side="left", fill="y")
+#         # self.preview_frame.pack(side="left", expand=True, fill="both")
+#         # self.photobook_frame.pack(side="right", fill="y")
+#
+#     def create_widgets(self):
+#         pass
 
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Ed Sheeran Photo Editor")
-        self.geometry("1200x700")
+# class ImageEditor:
+#     # Note: we can also grayscale the image to further edit them, like the
+#     # contrast etc. BUT my concern is making the Ed too editable will drive the
+#     # user away from editing the actual card
+#     # (which is also Prof's concern from OH)
+#     def __init__(self, master):
+#         self.master = master
+#         master.title("Image Editor")
+#
+#         self.image = None  # in the form of file opened
+#         self.photo_image = None  # ImageTk.PhotoImage(self.image)
+#         self.canvas = tk.Canvas(master, bg="lightgray")
+#         self.canvas.pack(fill=tk.BOTH, expand=True)
+#
+#         self.create_widgets()
+#
+#     def create_widgets(self):
+#         # buttons that triggers the change of chosen photo_image
+#         # trigger -> command=self.display_image
+#         pass
+#
+#     def display_image(self):
+#         self.open_image()
+#         if self.image:
+#             self.photo_image = ImageTk.PhotoImage(self.image)
+#         pass
+#
+#     def open_image(self):
+#         # update (Properties.state) with the chosen image
+#         # get image from one of the Ed Pict, from properties.py class object
+#         pass
+#
+# root = App()
+# root.mainloop()
 
-        self.card = None  # shared model
-
-        # self.attributes_frame = AttributesFrame()
-        # self.preview_frame = PreviewFrame(self)
-        # self.photobook_frame = PhotobookFrame(self)
-
-        # self.attributes_frame.pack(side="left", fill="y")
-        # self.preview_frame.pack(side="left", expand=True, fill="both")
-        # self.photobook_frame.pack(side="right", fill="y")
-
-    def create_widgets(self):
-        pass
-
-class ImageEditor:
-    # Note: we can also grayscale the image to further edit them, like the
-    # contrast etc. BUT my concern is making the Ed too editable will drive the
-    # user away from editing the actual card
-    # (which is also Prof's concern from OH)
-    def __init__(self, master):
-        self.master = master
-        master.title("Image Editor")
-
-        self.image = None  # in the form of file opened
-        self.photo_image = None  # ImageTk.PhotoImage(self.image)
-        self.canvas = tk.Canvas(master, bg="lightgray")
-        self.canvas.pack(fill=tk.BOTH, expand=True)
-
-        self.create_widgets()
-
-    def create_widgets(self):
-        # buttons that triggers the change of chosen photo_image
-        # trigger -> command=self.display_image
-        pass
-
-    def display_image(self):
-        self.open_image()
-        if self.image:
-            self.photo_image = ImageTk.PhotoImage(self.image)
-        pass
-
-    def open_image(self):
-        # update (Properties.state) with the chosen image
-        # get image from one of the Ed Pict, from properties.py class object
-        pass
-
-root = App()
-root.mainloop()
+from EditSheeran.views.view import
---
+++ views/view.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+
+root = tk.Tk()
+root.geometry("1200x700")
+root.title("EditSheeran")
+
+root.iconbitmap("../assets/ed.ico")
+
+root.mainloop()
