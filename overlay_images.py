9fa66c3ade52fe1e9c7080385dbb9e1f75cc9d17
Michael Chininea <82243647+Twilii@users.noreply.github.com>
2025-11-25 18:18:12 -0500

    uwu

--- overlay_images.py
+++ overlay_images.py
@@ -4,9 +4,9 @@ from tkinter import PhotoImage
 BASE_DIR = os.path.dirname(__file__)
 
 eyes = {
-"heart eyes": os.path.join(BASE_DIR, "assets", "HEART_EYES.png"),
+    "heart eyes": os.path.join(BASE_DIR, "assets", "HEART_EYES.png"),
     "blue eyes": os.path.join(BASE_DIR, "assets", "BIG_BLUE_EYES.png"),
-    "brown eyes": os.path.join(BASE_DIR, "assets", "BROWN_EYES_.png")
+    "brown eyes": os.path.join(BASE_DIR, "assets", "BROWN_EYES.png")
 }
 
 mouths = {
