17d586d1ee13c293a33862853ba1d38f64bdf250
sanpieee <sanpai.tsia@gmail.com>
2025-11-26 21:26:02 -0500

    Update attribute_frame.py

--- views/attribute_frame.py
+++ views/attribute_frame.py
@@ -9,7 +9,44 @@ from controllers.controller import Controller
 
 
 class EdSelector(tk.Frame):
-    pass
+    def __init__(self, parent, library):
+        super().__init__(parent)
+        self.library = library
+        self.keys = list(library.keys())
+        self.index = 0
+        for k, path in eds.items():
+            pil_img = Image.open(path).resize((100, 100))
+            self.library[k] = ImageTk.PhotoImage(pil_img)
+
+        self.btn_prev = tk.Button(self, text="◀", command=self.prev)
+        self.btn_prev.grid(row=1, column=0, padx=5)
+
+        self.label = tk.Label(self)
+        self.label.grid(row=1, column=1, padx=5)
+        self.label2 = tk.Label(self, text="Eds")
+        self.label2.grid(row=0, column=1, padx=5)
+
+        self.btn_next = tk.Button(self, text="▶", command=self.next)
+        self.btn_next.grid(row=1, column=2, padx=5)
+
+        self.update_image()
+
+    def update_image(self):
+        key = self.keys[self.index]
+        img = self.library[key]
+        self.label.img = img  # keep reference
+        self.label.config(image=img)
+
+    def next(self):
+        self.index = (self.index + 1) % len(self.keys)
+        current_key = self.keys[self.index]
+        self.label.config(image=self.library[current_key])
+
+    def prev(self):
+        self.index = (self.index - 1) % len(self.keys)
+        current_key = self.keys[self.index]
+        self.label.config(image=self.library[current_key])
+
 
 class EyeSelector(tk.Frame):
     def __init__(self, parent, library):
