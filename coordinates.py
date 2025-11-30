8f55cb3b4be2821e1830ba10df7a45d7b3fe183d
Stanley <enderguy292@gmail.com>
2025-11-30 00:11:26 -0500

    resize coordinates and some images

--- coordinates.py
+++ coordinates.py
@@ -1,19 +1,19 @@
 ED_COORDS = {
     "ed_normal.png": {
-        "eyes":  (120, 380),
-        "mouth": (267, 257),
-        "accessory": (254, 115)
+        "eyes":  (120, 378),
+        "mouth": (120, 430),
+        "accessory": (50, 190)
     },
 
     "ed_monkey.png": {
-        "eyes":  (162, 126),
-        "mouth": (169, 176),
-        "accessory": (154, 67)
+        "eyes":  (60, 298),
+        "mouth": (70, 340),
+        "accessory": (-10, 110)
     },
 
     "ed_speaking.png": {
-        "eyes":  (238, 146),
-        "mouth": (234, 220),
-        "accessory": (244, 69)
+        "eyes":  (120, 290),
+        "mouth": (120, 360),
+        "accessory": (60, 120)
     },
 }
